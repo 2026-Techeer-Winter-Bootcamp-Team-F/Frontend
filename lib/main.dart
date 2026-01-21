750077f81c43461e824fed63c87a3b8cf8759439
space <kh3367@naver.com>
2026-01-21 23:02:36 +0900

    feat: 홈페이지 개편 및 거래 상세 기능 추가
    - 홈페이지 UI 전면 개편 (월별 누적/주간/월간 지출 차트)
    - 카테고리별 지출 분석 페이지 추가
    - 거래 내역 및 상세 페이지 구현
    - 하단 네비게이션 바 3탭으로 단순화 (홈/카드/구독)
    - 시스템 폰트 적용으로 웹 폰트 로딩 에러 해결
    - 인터랙티브 도넛 차트 및 라인 차트 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:my_app/config/theme.dart';
 import 'package:my_app/screens/login/login_page.dart'; // 수정: auth -> login 폴더로 변경
 import 'package:my_app/screens/splash/splash_page.dart';
 
 void main() {
+  // 웹에서 Google Fonts 자동 로딩 방지
+  if (kIsWeb) {
+    // 웹 환경에서 추가 설정이 필요한 경우 여기에 작성
+  }
   runApp(const MyApp());
 }
 
--- lib/screens/main_navigation.dart
+++ lib/screens/main_navigation.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/screens/home/home_page.dart';
-import 'package:my_app/screens/analysis/expense_analysis_page.dart';
 import 'package:my_app/screens/cards/card_analysis_page.dart';
 import 'package:my_app/screens/subscription/subscription_page.dart';
-import 'package:my_app/screens/chat/chat_page.dart';
 
 class MainNavigation extends StatefulWidget {
   const MainNavigation({super.key});
@@ -17,10 +15,8 @@ class _MainNavigationState extends State<MainNavigation> {
 
   final List<Widget> _pages = [
     const HomePage(),
-    const ExpenseAnalysisPage(),
     const CardAnalysisPage(),
     const SubscriptionPage(),
-    const ChatPage(),
   ];
 
   @override
@@ -37,6 +33,11 @@ class _MainNavigationState extends State<MainNavigation> {
             _currentIndex = index;
           });
         },
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: const Color(0xFF2196F3),
+        unselectedItemColor: Colors.grey,
+        selectedFontSize: 12,
+        unselectedFontSize: 12,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
@@ -44,24 +45,14 @@ class _MainNavigationState extends State<MainNavigation> {
             label: '홈',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.pie_chart_outline),
-            activeIcon: Icon(Icons.pie_chart),
-            label: '지출분석',
-          ),
-          BottomNavigationBarItem(
             icon: Icon(Icons.credit_card_outlined),
             activeIcon: Icon(Icons.credit_card),
             label: '카드',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.subscriptions_outlined),
-            activeIcon: Icon(Icons.subscriptions),
+            icon: Icon(Icons.receipt_long_outlined),
+            activeIcon: Icon(Icons.receipt_long),
             label: '구독',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat_bubble_outline),
-            activeIcon: Icon(Icons.chat_bubble),
-            label: 'AI상담',
           ),
         ],
       ),
