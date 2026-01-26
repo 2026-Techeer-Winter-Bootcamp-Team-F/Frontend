55ec9786ab433c148bfaff04ae587feadf9109f3
jeongsu2 <sweetpea427@naver.com>
2026-01-27 03:18:36 +0900

    fix:main.dart최적화

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' show kIsWeb, kReleaseMode; // ★ kReleaseMode 추가
-import 'package:device_preview/device_preview.dart'; // ★ 추가
+import 'package:flutter/foundation.dart' show kReleaseMode;
+import 'package:device_preview/device_preview.dart';
 import 'package:my_app/config/theme.dart';
 import 'package:my_app/screens/splash/splash_page.dart';
 
 void main() {
-  // 웹에서 Google Fonts 자동 로딩 방지
-  if (kIsWeb) {
-    // 웹 환경에서 추가 설정이 필요한 경우 여기에 작성
-  }
-
   runApp(
-    DevicePreview( // ★ 추가
-      enabled: !kReleaseMode, // ★ 디버그에서만 활성화
-      builder: (context) => const MyApp(),
-    ),
+    kReleaseMode
+        ? const MyApp()
+        : DevicePreview(
+            enabled: true,
+            builder: (context) => const MyApp(),
+          ),
   );
 }
 
@@ -28,9 +25,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       home: const SplashPage(),
-
-      // ★ DevicePreview 적용: appBuilder만 사용
-      builder: DevicePreview.appBuilder,
+      builder: kReleaseMode ? null : DevicePreview.appBuilder,
     );
   }
 }
